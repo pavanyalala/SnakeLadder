3e76b51b9bf00f34e1d11faa46c0d83e5fe0adc5
pavanyalala <pavanyalala4508@gmail.com>
2021-07-21 21:47:42 +0530

    [pavan]:number of times dice rolled is counted

--- SnakeAndLadderGame/SnakeLadder.cs
+++ SnakeAndLadderGame/SnakeLadder.cs
@@ -9,24 +9,27 @@ namespace SnakeAndLadderGame
         public void PlayerMoves()//method gives palyer to go head or not
         {
             int startPoint = 0;
+            int count = 0;
+
             while(startPoint != 100)
             {
-                    Random random = new Random();
-                    int dice = random.Next(1, 7);
-                    Console.WriteLine("Dice Value :"+dice);
-                   
-                    int choice = random.Next(0, 3);
-                    Console.WriteLine("Choice :"+choice);
-                    switch (choice)
-                    {
-                        case 0://0 refers to no play
-                            startPoint = startPoint;
-                            Console.WriteLine("Player will be remain at same position ");
-                            break;
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                Console.WriteLine("Dice Value :"+dice);
+                count++;
+                Console.WriteLine("Number of times Dice Rolled " + count);
+                int choice = random.Next(0, 3);
+                Console.WriteLine("Choice :"+choice);
+                switch (choice)
+                {         
+                    case 0://0 refers to no play
+                        startPoint = startPoint;
+                        Console.WriteLine("Player will be remain at same position ");
+                        break;
 
-                        case 1://1 refers to ladder
-                            startPoint=startPoint+dice;
-                            Console.WriteLine("Player moved by position "+startPoint);
+                    case 1://1 refers to ladder
+                         startPoint=startPoint+dice;
+                         Console.WriteLine("Player moved by position "+startPoint);
                             if (startPoint > 100)
                             {
                                 startPoint = startPoint - dice;
@@ -37,21 +40,18 @@ namespace SnakeAndLadderGame
                             }
                             break;
 
-                        case 2://refers to snake
-                            startPoint = startPoint - dice;
-                            Console.WriteLine("Player dropped by position "+startPoint);
+                     case 2://refers to snake
+                          startPoint = startPoint - dice;
+                          Console.WriteLine("Player dropped by position "+startPoint);
                             if (startPoint < 0)
                             {
                                 startPoint = 0;
                             }
                             Console.WriteLine("You got the Zero value Restart the game");
                             break;
-                    }
-                    
-
+                }
             }
-                Console.WriteLine("Current Position of Player : " + startPoint);
-            
+            Console.WriteLine("Current Position of Player : " + startPoint);
         }
     }
 }
