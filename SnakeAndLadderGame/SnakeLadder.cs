69625ebb31a25fa0b925168d6c4ca19fd84d3046
pavanyalala <pavanyalala4508@gmail.com>
2021-07-21 18:47:38 +0530

    [pavan]:Player position is changes

--- SnakeAndLadderGame/SnakeLadder.cs
+++ SnakeAndLadderGame/SnakeLadder.cs
@@ -5,14 +5,30 @@ using System.Text;
 namespace SnakeAndLadderGame
 {
     class SnakeLadder
-    {
-        //variables
-        int startPoint = 0;
-        public void RollDice()//method gives dice value
+    {  
+        public void PlayerChecks()//method gives palyer to go head or not
         {
+            int startPoint = 0;
             Random random  = new Random();
             int dice = random.Next(1, 7);//1-6 dice values
-            Console.WriteLine("Rolled Dice Value : " + dice);
+            int choice = random.Next(0, 3);//0,1,2 for choices
+            switch (choice)
+            {
+                case 0://0 refer to no play
+                    startPoint = startPoint ;
+                    Console.WriteLine("Player remains at same position " + startPoint);
+                    break;
+
+                case 1://refer to ladder 
+                    startPoint = startPoint + dice;
+                    break;
+
+                case 2://refer to snake
+                    startPoint = startPoint - dice;
+                    break;
+
+            }
+            Console.WriteLine("Current Position of Player : "+ startPoint);
                 
         }
     }
