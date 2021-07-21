8891fb7a0c152cd7727de87e69b068af53b85d8a
pavanyalala <pavanyalala4508@gmail.com>
2021-07-21 19:45:56 +0530

    [pavan] Player to raech 100 is initialized

--- SnakeAndLadderGame/SnakeLadder.cs
+++ SnakeAndLadderGame/SnakeLadder.cs
@@ -5,31 +5,35 @@ using System.Text;
 namespace SnakeAndLadderGame
 {
     class SnakeLadder
-    {  
-        public void PlayerChecks()//method gives palyer to go head or not
+    {
+        public void PlayerMoves()//method gives palyer to go head or not
         {
             int startPoint = 0;
-            Random random  = new Random();
-            int dice = random.Next(1, 7);//1-6 dice values
-            int choice = random.Next(0, 3);//0,1,2 for choices
-            switch (choice)
-            {
-                case 0://0 refer to no play
-                    startPoint = startPoint ;
-                    Console.WriteLine("Player remains at same position " + startPoint);
-                    break;
 
-                case 1://refer to ladder 
-                    startPoint = startPoint + dice;
-                    break;
+          
+                while(startPoint <= 100)
+                {
+                    Random random = new Random();
+                    int dice = random.Next(1, 7);
+                    int choice = random.Next(0, 3);
+                    switch (choice)
+                    {
+                        case 0://0 refers to no play
+                            startPoint = startPoint;
+                            break;
+                        case 1://1 refers to ladder
+                            startPoint=startPoint+dice;
+                            break;
+                        case 2://refers to snake
+                        startPoint = startPoint - dice;
+                        if (startPoint < 0)
+                            startPoint = 0;
+                        break;
+                    }
 
-                case 2://refer to snake
-                    startPoint = startPoint - dice;
-                    break;
-
-            }
-            Console.WriteLine("Current Position of Player : "+ startPoint);
-                
+                }
+                Console.WriteLine("Current Position of Player : " + startPoint);
+            
         }
     }
 }
