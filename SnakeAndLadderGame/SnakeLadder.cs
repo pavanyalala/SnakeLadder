2343cf5c06000ec7940e6519f6bd39f229734c4e
pavanyalala <pavanyalala4508@gmail.com>
2021-07-22 12:06:23 +0530

    [pavan]:Refactored to Constant variables

--- SnakeAndLadderGame/SnakeLadder.cs
+++ SnakeAndLadderGame/SnakeLadder.cs
@@ -8,6 +8,11 @@ namespace SnakeAndLadderGame
     {
         public void PlayerMoves()//method gives palyer to go head or not
         {
+            //constant variables
+            const int NOPLAY = 0;
+            const int LADDER = 1;
+            const int SNAKE = 2;
+
             int startPoint = 0;
             int count = 0;
 
@@ -17,17 +22,17 @@ namespace SnakeAndLadderGame
                 int dice = random.Next(1, 7);
                 Console.WriteLine("Dice Value :"+dice);
                 count++;
-                Console.WriteLine("Number of times Dice Rolled " + count);
+                Console.WriteLine("Number of times Dice Rolled : " + count);
                 int choice = random.Next(0, 3);
                 Console.WriteLine("Choice :"+choice);
                 switch (choice)
                 {         
-                    case 0://0 refers to no play
-                        startPoint = startPoint;
-                        Console.WriteLine("Player will be remain at same position ");
-                        break;
+                    case NOPLAY://0 refers to no play
+                         startPoint = startPoint;
+                         Console.WriteLine("Player will be remain at same position ");
+                         break;
 
-                    case 1://1 refers to ladder
+                    case LADDER://1 refers to ladder
                          startPoint=startPoint+dice;
                          Console.WriteLine("Player moved by position "+startPoint);
                             if (startPoint > 100)
@@ -40,7 +45,7 @@ namespace SnakeAndLadderGame
                             }
                             break;
 
-                     case 2://refers to snake
+                     case SNAKE://refers to snake
                           startPoint = startPoint - dice;
                           Console.WriteLine("Player dropped by position "+startPoint);
                             if (startPoint < 0)
